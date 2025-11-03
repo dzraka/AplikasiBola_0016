b667baaec2a84c7ce1c4253590a442f93f807ee7
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2025-11-03 11:23:13 +0700

    update

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -19,137 +19,148 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFE3F2FD),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.sports_soccer,
-                      size: 80,
-                      color: Color(0xFF42A5F5),
-                    ),
-                  ],
-                ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF0823CA), Color(0xFFBB0854)],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png', width: 120),
 
-                const SizedBox(height: 40),
+                  const SizedBox(height: 40),
 
-                TextFormField(
-                  controller: _emailCtr,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "email tidak boleh kosong";
-                    } else if (!value.contains("@")) {
-                      return "harus dengan format @";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    label: Text("Email"),
-                    hintText: "masukkan email, ex: nama@mail.com",
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                ),
-
-                const SizedBox(height: 20),
-
-                TextFormField(
-                  controller: _pwdCtr,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "password tidak boleh kosong";
-                    } else if (value.length < 8) {
-                      return "password harus terdiri dari minimal 8 karakter";
-                    }
-                    return null;
-                  },
-                  obscureText: isObscure,
-                  keyboardType: TextInputType.visiblePassword,
-                  decoration: InputDecoration(
-                    label: Text("Password"),
-                    hintText: "masukkan password",
-                    filled: true,
-                    fillColor: Colors.white,
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        isObscure ? Icons.visibility_off : Icons.visibility,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          isObscure = !isObscure;
-                        });
-                      },
-                    ),
-                  ),
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                ),
-
-                const SizedBox(height: 30),
-
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeScreen()),
-                        );
+                  TextFormField(
+                    controller: _emailCtr,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "email tidak boleh kosong";
+                      } else if (!value.contains("@")) {
+                        return "harus dengan format @";
                       }
+                      return null;
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF42A5F5),
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      label: Text("Email"),
+                      hintText: "masukkan email, ex: nama@mail.com",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20,
-                      ),
-                    ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
                   ),
-                ),
 
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Belum memiliki akun? "),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
-                          ),
-                        );
+                  TextFormField(
+                    controller: _pwdCtr,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "password tidak boleh kosong";
+                      } else if (value.length < 8) {
+                        return "password harus terdiri dari minimal 8 karakter";
+                      }
+                      return null;
+                    },
+                    obscureText: isObscure,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      label: Text("Password"),
+                      hintText: "masukkan password",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          isObscure ? Icons.visibility_off : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isObscure = !isObscure;
+                          });
+                        },
+                      ),
+                    ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        }
                       },
-                      child: const Text(
-                        "Daftar",
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF6A1B9A),
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        "Login",
                         style: TextStyle(
-                          color: Colors.blueAccent,
-                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Belum memiliki akun? ",
+                        style: TextStyle(color: Colors.white70, fontSize: 16),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Daftar",
+                          style: TextStyle(
+                            color: Color(0xFFEEBC00),
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
